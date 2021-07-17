7c2da866f38ec04bd856adc0220753791dbb6f51
Rodrigo <rodrigo.hammes7@gmail.com>
2021-07-16 21:45:29 -0300

    add eslint e editorconfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello world')
+  response.send('Hello world');
 });
 
 app.listen(3333, () => console.log('Server stated at http://localhost:3333'));
